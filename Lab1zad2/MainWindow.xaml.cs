8ade7d0e072f58359c1ab7b8fc052be1179a220b
Pbis778 <pb305347@student.polsl.pl>
2024-03-09 18:19:16 +0100

    com_zad2

---
+++ Lab1zad2/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab1zad2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPiramida_Click(object sender, RoutedEventArgs e)
+        {
+            int rows = 8;
+            double stepWidth = 20;
+            double stepHeight = 20;
+
+            double startX = (canvas.ActualWidth - rows * stepWidth) / 2;
+            double startY = 50;
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j=0; j <= i; j++)
+                {
+                    double x = startX + j * stepWidth;
+                    double y = startY + i * stepHeight;
+                    Rectangle rect = new Rectangle();
+                    rect.Width = stepWidth;
+                    rect.Height = stepHeight;
+                    rect.Fill = Brushes.Black;
+
+                    Canvas.SetLeft(rect, x);
+                    Canvas.SetTop(rect, y);
+                    canvas.Children.Add(rect);
+                }
+            }
+        }
+    }
+}
