3cb3bdabe65b7c396cea8c2b7e8f964f0d4fb38a
PioBis778 <pb305347@student.polsl.pl>
2024-03-10 19:38:22 +0100

    com_zad3

---
+++ Lab1zad3/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab1zad3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            drawChessboard();
+        }
+
+        private void drawChessboard()
+        {
+            int rows = 8;
+            int cols = 8;
+            double squareSize = 50;
+
+            for (int i = 0; i < rows; i++)
+            {
+                var rowDefinition = new RowDefinition();
+                rowDefinition.Height = new GridLength(squareSize);
+                grid.RowDefinitions.Add(rowDefinition);
+            }
+
+            for (int i = 0; i < cols; i++)
+            {
+                var colDefinition = new ColumnDefinition();
+                colDefinition.Width = new GridLength(squareSize);
+                grid.ColumnDefinitions.Add(colDefinition);
+            }
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Rectangle rect = new Rectangle();
+                    rect.Width = squareSize;
+                    rect.Height = squareSize;
+                    rect.Fill = (i + j) % 2 == 0 ? Brushes.White : Brushes.Black;
+
+                    Grid.SetRow(rect, i);
+                    Grid.SetColumn(rect, j);
+
+                    grid.Children.Add(rect);
+                }
+            }
+        }
+    }
+}
